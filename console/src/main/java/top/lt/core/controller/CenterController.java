72435b2ee63ba8020c11007e6320980301bb9bc9
LittleTry <ht@yuancore.com>
2017-10-27 19:23:19 +0800

    搭建后台管理页面
    Signed-off-by: LittleTry <ht@yuancore.com>

--- console/src/main/java/top/lt/core/controller/CenterController.java
+++ console/src/main/java/top/lt/core/controller/CenterController.java
@@ -14,22 +14,48 @@ import java.util.Date;
  * @date 2017-10-27
  */
 @Controller
+@RequestMapping(value = "/control")
 public class CenterController {
 
-    @Autowired
-    private TestTbService testTbService;
     //入口
-    /**
-     * ModelAndView  : 跳转视图+数据     不用
-     * void  : 异步时 ajax
-     * String  : 跳转视图      +  Model
-     */
-    @RequestMapping(value = "/test/index.do")
+    @RequestMapping(value = "/index.do")
     public String index(Model model){
-        TestTb testTb = new TestTb();
-        testTb.setName("刘亦菲");
-        testTb.setBirthday(new Date());
-        testTbService.insertTestTb(testTb);
+
         return "index";
+    }
+    //头
+    @RequestMapping(value = "/top.do")
+    public String top(Model model){
+
+        return "top";
+    }
+    //身体
+    @RequestMapping(value = "/main.do")
+    public String main(Model model){
+
+        return "main";
+    }
+    //身体--左
+    @RequestMapping(value = "/left.do")
+    public String left(Model model){
+
+        return "left";
+    }
+    //身体-右
+    @RequestMapping(value = "/right.do")
+    public String right(Model model){
+
+        return "right";
+    }
+    //商品身体-
+    @RequestMapping(value = "/frame/product_main.do")
+    public String product_main(Model model){
+
+        return "frame/product_main";
+    }
+    @RequestMapping(value = "/frame/product_left.do")
+    public String product_left(Model model){
+
+        return "frame/product_left";
     }
 }
