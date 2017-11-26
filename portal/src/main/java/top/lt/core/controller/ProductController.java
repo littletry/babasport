bd834e4c74f60e6bc7bb1a3eaedadf5d7d6b5cda
LittleTry <ht@yuancore.com>
2017-11-27 03:26:56 +0800

    前台商品搜索的主流程走通
    Signed-off-by: LittleTry <ht@yuancore.com>

--- portal/src/main/java/top/lt/core/controller/ProductController.java
+++ portal/src/main/java/top/lt/core/controller/ProductController.java
@@ -1,7 +1,13 @@
 package top.lt.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.lt.core.bean.product.Product;
+import top.lt.core.service.SearchService;
+
+import java.util.List;
 
 /**     前台商品
  * @author LittleTry
@@ -13,5 +19,14 @@ public class ProductController {
     @RequestMapping(value = "/")
     public String index(){
         return "index";
+    }
+    @Autowired
+    private SearchService searchService;
+    //搜索
+    @RequestMapping(value = "/search")
+    public String search(String keyword, Model model) throws Exception {
+        List<Product> products = searchService.selectProductListByQuery(keyword);
+        model.addAttribute("products",products);
+        return "search";
     }
 }
